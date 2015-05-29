872ea030194e513ecda2b32ca5b9d086806bca67
Gix075 <gildo.giuliani@gmail.com>
2015-05-29 21:52:45 +0200

    Esportazione automatica MDtoHTML
    Implementato GRUNT per l'esportazione automatica dei file da markdown a
    html

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,18 +29,39 @@ module.exports = function(grunt) {
             }
         },
         
+        dom_munger: {
+            clean_ids: {
+            options: {
+                update: [
+                    {selector:'h1',attribute:'id', value:'none'},
+                    {selector:'h2',attribute:'id', value:'none'},
+                    {selector:'h3',attribute:'id', value:'none'},
+                    {selector:'h4',attribute:'id', value:'none'},
+                    {selector:'h5',attribute:'id', value:'none'},
+                    {selector:'h6',attribute:'id', value:'none'}
+                ]
+            },
+            expand: true,    
+            cwd: 'docs/',    
+            src: '**/*.html', //could be an array of files 
+            dest: 'docs/' //optional, if not specified the src file will be overwritten 
+            }
+        },
+        
         'string-replace': {
             dist: {
-                files: {
+                files: [{
                     expand: true,
                     cwd: 'docs/',
                     src: '**/*.html',
                     dest: 'docs/'
-                },
+                }],
                 options: {
+                    saveUnchanged: true,
                     replacements: [
                         {
-                            pattern: /<h1 (\\S+)=[\"']?((?:.(?![\"']?\\s+(?:\\S+)=|[>\"']))+.)[\"']>?/,
+                            //pattern: /<h1 (\\S+)=[\"']?((?:.(?![\"']?\\s+(?:\\S+)=|[>\"']))+.)[\"']>?/,
+                            pattern: / id="none"/g,
                             replacement: ''
                         }
                     ]
@@ -54,12 +75,14 @@ module.exports = function(grunt) {
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-md2html');
+    grunt.loadNpmTasks('grunt-dom-munger');
     grunt.loadNpmTasks('grunt-string-replace');
 
     // Default task(s).
     grunt.registerTask('default', [
         'clean',
         'md2html',
+        'dom_munger',
         'string-replace'
     ]);
 
