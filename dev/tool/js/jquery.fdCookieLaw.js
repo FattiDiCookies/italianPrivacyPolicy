94dcfe323fdae86250fe2ba594fe2dd13a2b847d
Gildo Giuliani <gildo.giuliani@gmail.com>
2016-11-04 01:00:49 +0100

    v1.4.0
    version 1.4.0

--- dev/tool/js/jquery.fdCookieLaw.js
+++ dev/tool/js/jquery.fdCookieLaw.js
@@ -731,10 +731,15 @@
                         if($.inArray(service, currentCookie) === -1) currentCookie.push(service);
                         callbackData.allowed_services = currentCookie;
                         newCookieValue = currentCookie.toString(); 
+                        // add single service cookie
+                        plugin.writeCookie(config.cookieBanner.cookieName + '_' + service, "true", config.cookieBanner.cookieExpire);
+                        
                     }else{
                         callbackData.allowed_services = [service];
                         newCookieValue = service;  
                         plugin.cookieAccept(plugin, cookieData, false);
+                        // add single service cookie
+                        plugin.writeCookie(config.cookieBanner.cookieName + '_' + service, "true", config.cookieBanner.cookieExpire);
                     }
                     
                     break;
@@ -748,11 +753,12 @@
                         }    
                         callbackData.allowed_services = currentCookie;
                         newCookieValue = currentCookie.toString(); 
-                        if (newCookieValue === "") {
+                        if (newCookieValue === "") {  
                             callbackData.allowed_services = [];
                             newCookieValue = cookieData.cvalue_rejected;
-                            plugin.cookieReject(plugin, cookieData);
+                            plugin.cookieReject(plugin, cookieData);        
                         }
+                        plugin.writeCookie(config.cookieBanner.cookieName + '_' + service, "false", config.cookieBanner.cookieExpire);
                         
                     }
                     break;
@@ -774,7 +780,7 @@
             
             var servicesCookieName = cookieName + "_services";
             
-            if(action === true) {
+            
                 
                 $.ajax({
                     url: plugin.settings.config,
@@ -782,14 +788,23 @@
                     success: function (config) {
                         // @DEBUG 
                         if(plugin.settings.debug === true) console.log(pluginName + ': servicesChoise_acceptAll() -> config loaded!');
-
-                        $.each(config.cookiePolicy.services, function(index) {
-                            if (this.active === true) { 
+                        if(action === true) {
+                            $.each(config.cookiePolicy.services, function(index) {
+                                if (this.active === true) { 
+                                    $.each(config.cookiePolicy.services[index].services, function(key, value) {
+                                        if(value === true) plugin.serviceChoise_handleServiceCookie(plugin, config, key, "add", false);
+                                    });
+                                }
+                            });
+                        }else{
+                            plugin.writeCookie(servicesCookieName, 'rejected', 365);
+                            $.each(config.cookiePolicy.services, function(index) {
                                 $.each(config.cookiePolicy.services[index].services, function(key, value) {
-                                    if(value === true) plugin.serviceChoise_handleServiceCookie(plugin, config, key, "add", false);
+                                    plugin.serviceChoise_handleServiceCookie(plugin, config, key, "remove", false);
+                                    console.log(key)
                                 });
-                            }
-                        });
+                            });
+                        }
 
                     },
                     error: function () {
@@ -798,9 +813,7 @@
                     }
                 });
                 
-            }else{
-                plugin.writeCookie(servicesCookieName, 'rejected', 365);
-            }
+            
             // get configuration file
             
         },
